82c8f8bfd07d61a4cdda891d1087ee7656a320f8
wyl199188 <wyl199188@126.com>
2026-03-10 11:12:03 +0800

    首页轮播图的搜索框和指示器的实现

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,32 +11,98 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
-  List<Widget> _getPageChildren(){
+  final CarouselSliderController _carouselSliderController =
+      CarouselSliderController();
+  int _currentIndex = 0;
+
+  List<Widget> _getPageChildren() {
     double screenWidth = MediaQuery.of(context).size.width;
-    return List.generate(widget.bannarList.length, (int index){
-      return Image.network(widget.bannarList[index].imageUrl,fit: BoxFit.cover,width: screenWidth);
+    return List.generate(widget.bannarList.length, (int index) {
+      return Image.network(
+        widget.bannarList[index].imageUrl,
+        fit: BoxFit.cover,
+        width: screenWidth,
+      );
     });
   }
 
-  Widget _getSlider(){
-    return CarouselSlider(items: _getPageChildren(),
-    options: CarouselOptions(height:300,
-    viewportFraction: 1,
-    autoPlay: true,
-    autoPlayInterval: Duration(seconds: 1)));
+  Widget _getSlider() {
+    return CarouselSlider(
+      carouselController: _carouselSliderController,
+      items: _getPageChildren(),
+      options: CarouselOptions(
+        height: 300,
+        viewportFraction: 1,
+        autoPlay: false,
+        onPageChanged: (index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
+        autoPlayInterval: Duration(seconds: 3),
+      ),
+    );
   }
 
-  List<Widget> _getStackChildren(){
-    return [
-      // TextField(decoration: BoxDecoration(color: Colors.blueGrey)),
-      _getSlider(),
-    ];
+  Widget _getSearch() {
+    return Positioned(
+      left: 10,
+      right: 10,
+      top: 10,
+      child: Container(
+        alignment: Alignment.centerLeft,
+        width: double.infinity,
+        height: 50,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(0, 0, 0, 0.4),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          "搜索...",
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _getIndicator() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 5,
+      child: SizedBox(
+        height: 20,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannarList.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                _carouselSliderController.jumpToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                  width: _currentIndex == index ? 30 : 20,
+                  height: 6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: _currentIndex == index ? Colors.blue : Colors.white,
+                  ),
+                  margin: EdgeInsets.only(right: 10),
+                ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _getStackChildren() {
+    return [_getSlider(), _getSearch(), _getIndicator()];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: _getStackChildren(),
-    );
+    return Stack(children: _getStackChildren());
   }
 }
